a9eba1f03760f3a33eb225fb1760c4ea0e517f38
Ben Gimbel <BenGimbel@Bens-MacBook-Pro.local>
2018-12-14 15:15:55 -0600

    cleaned up code

--- src/Components/ListView.js
+++ src/Components/ListView.js
@@ -5,57 +5,42 @@ import { bindActionCreators } from 'redux';
 import { List, ListItem } from 'react-native-elements';
 
 
-export default class BellyListView extends Component<Props> {
-constructor(props){
-    super(props)
-    console.log('ehy')
-    // this.state = ({
-        
-    // })
-}
-
-renderList(){
-    
-    if(this.props.data){
-        console.log(this.props.data, 'dataprops')
-        // let milesUnformatted = biz.distance * 0.000621371192
-        // let miles = Math.round(milesUnformatted * 100) / 100
-        return (
-            // <Text>{this.props.data[0].name}</Text>
-        <View>
-            {/* <List> */}
-            <FlatList
-                data={this.props.data}
-                keyExtractor={item => item.alias}
-                renderItem={({ item }) => (
-                    // <ListItem
-                    // key={item.name}
-                    // roundAvatar
-                    // title={item.name}
-                    // subtitle={item.name}
-                    // avatar={{uri:item.image_url}}
-                    
-                    // />
-                    <View style={styles.container}>
-                        <View style={styles.imageContainerStyle}>
-                            <Image style={{width: 100, height: 100}} source={{uri: item.image_url}} />
-                        </View>
-                        <View style={styles.textContainerStyle}>
-                            <Text style={styles.mainText}>{item.name}</Text>
-                            <Text style={styles.secondaryText}>{Math.round((item.distance * 0.000621371192) * 100) / 100 + ' miles away'}</Text>
-                            <Text style={styles.secondaryText}>{item.categories.map(info => info.title + ' ')}</Text>
-                        </View>
-                    </View>
-                    
-                )}
-            />
-            {/* </List> */}
-        </View>
-        )
+class BellyListView extends Component<Props> {
+    constructor(props){
+        super(props)
+        console.log('ehy')
+        // this.state = ({
+            
+        // })
     }
-}
+
+    renderList(){
+        
+        if(this.props.data){
+            return (
+            <View>
+                <FlatList
+                    data={this.props.data}
+                    keyExtractor={item => item.alias}
+                    renderItem={({ item }) => (
+                        <View style={styles.container}>
+                            <View style={styles.imageContainerStyle}>
+                                <Image style={{width: 100, height: 100}} source={{uri: item.image_url}} />
+                            </View>
+                            <View style={styles.textContainerStyle}>
+                                <Text style={styles.mainText}>{item.name}</Text>
+                                <Text style={styles.secondaryText}>{Math.round((item.distance * 0.000621371192) * 100) / 100 + ' miles away'}</Text>
+                                <Text style={styles.secondaryText}>{item.categories.map(info => info.title + ' ')}</Text>
+                            </View>
+                        </View>
+                        
+                    )}
+                />
+            </View>
+            )
+        }
+    }
     render() {
-        // console.log(this.props.data, 'data');
         return (
             <View>
             {
@@ -65,7 +50,7 @@ renderList(){
         )
     }
 }
-// export default BellyListView;
+export default BellyListView;
 
 const styles = StyleSheet.create({
     container: {
@@ -75,15 +60,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'flex-start',
         backgroundColor: '#fff'
-    //   position: 'absolute',
-    //   top: 0,
-    //   left: 0,
-    //   right: 0,
-    //   bottom: 0,
-    //   justifyContent: 'flex-end',
-    //   alignItems: 'center'
-        // flexDirection: 'column',
-        // marginTop: 50
     },
     imageContainerStyle: {
         padding: 15
@@ -98,5 +74,4 @@ const styles = StyleSheet.create({
     secondaryText: {
         width: 225
     },
-    
 });
